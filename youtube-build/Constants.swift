3dd4016080186c6af39c38bb1a9ec2e5831c47a1
Landry Argabright <landryargabright1@gmail.com>
2020-06-16 09:30:23 -0500

    working API request

---
+++ youtube-build/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  youtube-build
+//
+//  Created by Ray Argabright on 6/15/20.
+//  Copyright © 2020 Landry Argabright. All rights reserved.
+//
+
+import Foundation
---
+++ youtube-build/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  youtube-build
+//
+//  Created by Ray Argabright on 6/15/20.
+//  Copyright © 2020 Landry Argabright. All rights reserved.
+//
+
+import Foundation
