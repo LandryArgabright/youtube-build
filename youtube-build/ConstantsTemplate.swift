45e83ac8e5869a7f50878726c7ab663f17e0ea9b
Landry Argabright <landryargabright1@gmail.com>
2020-06-16 11:15:38 -0500

    Modified .gitignore to swap unique constants to constants template

--- youtube-build/ConstantsTemplate.swift
+++ youtube-build/ConstantsTemplate.swift
@@ -1,17 +1,26 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-build
 //
-//  Created by Landry Argabright on 6/15/20.
+//  Created by Ray Argabright on 6/16/20.
 //  Copyright © 2020 Landry Argabright. All rights reserved.
 //
 
+/*
+ Add Personal Keys and uncomment code
+ */
+
+
 import Foundation
 
+
+
+/*
 struct Constants {
-    
-    static var API_KEY = "AIzaSyAGEgP8AT7cr9pBjpWenP6-Ex06RScg6dE"
-    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+ 
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants .PLAYLIST_ID)&key=\(Constants.API_KEY)"
-    
-}
+
+ }
+ */
